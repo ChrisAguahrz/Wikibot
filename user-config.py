6bef1fc404816825ee88b35fc99d34065c99bc5e
ChrisAguahrz <chrisaguahrz@gmail.com>
2026-04-27 18:38:49 +0300

    Final fix attempt

--- user-config.py
+++ user-config.py
@@ -1,25 +1,17 @@
-# -*- coding: utf-8 -*-
 import os
 
 family = 'wikipedia'
 mylang = 'sw'
 
-# Get credentials from environment
-username = os.getenv('WIKI_USERNAME', 'Gayle157')
-password = os.getenv('WIKI_PASSWORD', 'CiteBot@fp5d4lfqvjrgi8d4e84s8burfovakba9')
-
+# MUST use this exact format
 usernames = {}
 usernames['wikipedia'] = {}
-usernames['wikipedia']['sw'] = username
+usernames['wikipedia']['sw'] = os.getenv('WIKI_USERNAME', 'Gayle157')
 
-# Create a bot password file
-import os as _os
-_bot_pw_dir = _os.path.join(_os.path.expanduser('~'), '.pywikibot')
-_os.makedirs(_bot_pw_dir, exist_ok=True)
-_bot_pw_file = _os.path.join(_bot_pw_dir, 'botpassword')
-with open(_bot_pw_file, 'w') as f:
-    f.write(f'("{username}", "{password}")\n')
-
+# Use authenticate with password
 authenticate = {}
 authenticate['wikipedia'] = {}
-authenticate['wikipedia']['sw'] = (username, password)
+authenticate['wikipedia']['sw'] = (
+    os.getenv('WIKI_USERNAME', 'Gayle157'),
+    os.getenv('WIKI_PASSWORD', 'CiteBot@fp5d4lfqvjrgi8d4e84s8burfovakba9')
+)
