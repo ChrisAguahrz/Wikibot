50c647a46c9fb64325931780afb2df402903b86b
ChrisAguahrz <chrisaguahrz@gmail.com>
2026-04-27 18:29:20 +0300

    Fix password with botpassword file

--- user-config.py
+++ user-config.py
@@ -4,6 +4,7 @@ import os
 family = 'wikipedia'
 mylang = 'sw'
 
+# Get credentials from environment
 username = os.getenv('WIKI_USERNAME', 'Gayle157')
 password = os.getenv('WIKI_PASSWORD', 'CiteBot@fp5d4lfqvjrgi8d4e84s8burfovakba9')
 
@@ -11,13 +12,14 @@ usernames = {}
 usernames['wikipedia'] = {}
 usernames['wikipedia']['sw'] = username
 
-# Store password so pywikibot doesn't prompt
-_password = password
+# Create a bot password file
+import os as _os
+_bot_pw_dir = _os.path.join(_os.path.expanduser('~'), '.pywikibot')
+_os.makedirs(_bot_pw_dir, exist_ok=True)
+_bot_pw_file = _os.path.join(_bot_pw_dir, 'botpassword')
+with open(_bot_pw_file, 'w') as f:
+    f.write(f'("{username}", "{password}")\n')
 
 authenticate = {}
 authenticate['wikipedia'] = {}
 authenticate['wikipedia']['sw'] = (username, password)
-
-# CRITICAL: This tells pywikibot not to ask for password
-import pywikibot
-pywikibot.config.password_file = None
