b95a5b467fbdd5e75db7790581861fbed503afd2
ChrisAguahrz <chrisaguahrz@gmail.com>
2026-04-27 18:22:10 +0300

    Fix password prompt

--- user-config.py
+++ user-config.py
@@ -4,11 +4,20 @@ import os
 family = 'wikipedia'
 mylang = 'sw'
 
-# Direct credentials (for local use only)
+username = os.getenv('WIKI_USERNAME', 'Gayle157')
+password = os.getenv('WIKI_PASSWORD', 'CiteBot@fp5d4lfqvjrgi8d4e84s8burfovakba9')
+
 usernames = {}
 usernames['wikipedia'] = {}
-usernames['wikipedia']['sw'] = 'Gayle157'
+usernames['wikipedia']['sw'] = username
+
+# Store password so pywikibot doesn't prompt
+_password = password
 
 authenticate = {}
 authenticate['wikipedia'] = {}
-authenticate['wikipedia']['sw'] = ('Gayle157', 'CiteBot@fp5d4lfqvjrgi8d4e84s8burfovakba9')
+authenticate['wikipedia']['sw'] = (username, password)
+
+# CRITICAL: This tells pywikibot not to ask for password
+import pywikibot
+pywikibot.config.password_file = None
